b0819568adbca87317ee0013f443695c3dc453c5
longhoang94 <longsutls@gmail.com>
2018-08-21 08:51:04 +0700

    add program sum digonal-line

--- src/SumDiagonalLine.java
+++ src/SumDiagonalLine.java
@@ -1,39 +1,52 @@
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class SumDiagonalLine {
     public static void main(String[] args) {
-        for (int i = 0; i < getArray().length; i++) {
-            for (int j = 0; j < getArray(i).length; j++) {
-                System.out.println(getArray()[i][j]) + "/t");
-            }
-            System.out.println();
-        }
+        int[][] array = getSquareMatrix(getHeight(), getWidth());
+        displayArray(array);
+        System.out.println(getSumDiagonalLine(array));
     }
 
-    private static int[][] getArray() {
-        int[][] array = new int[getSize()][getSize()];
+    private static int[][] getSquareMatrix(int height, int width) {
+        int[][] array = new int[height][width];
         Random rd = new Random();
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = rd.nextInt(100);
+                array[i][j] = rd.nextInt(100) + 1;
             }
         }
         return array;
     }
 
-    private static int getSize() {
+    private static int getHeight() {
+        System.out.print("Nhập vào chiều rộng: ");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập vào 1 cạnh của hình vuông: ");
         return sc.nextInt();
     }
 
-    private static int getSumDionalLine(int[][] array) {
+    private static int getWidth() {
+        System.out.print("Nhập vào chiều dài: ");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    private static void displayArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int getSumDiagonalLine(int[][] array) {
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                sum +=array[i][j];
+                if (i == j) {
+                    sum += array[i][j];
+                }
             }
         }
         return sum;
